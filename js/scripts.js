a49fe50965073134816a0d39759dde5b68683c21
Molly-code-dev <mollymumbi42@gmail.com>
2020-07-17 15:59:40 +0300

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ function MakeArray(n) {
     }
 }
 days = new MakeArray(7);
-days[0] = "Sunday"
-days[1] = "Monday"
-days[2] = "Tuesday"
-days[3] = "Wednesday"
-days[4] = "Thursday"
-days[5] = "Friday"
-days[6] = "Friday"
+days[0] = "Sunday";
+days[1] = "Monday";
+days[2] = "Tuesday";
+days[3] = "Wednesday";
+days[4] = "Thursday";
+days[5] = "Friday";
+days[6] = "Friday";
 
 
 akanFemale = new MakeArray(7);
